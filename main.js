9ba5950ccb202919727b22b634aebdf3c9f9f51a
mug-coder <81393910+mug-coder@users.noreply.github.com>
2021-12-23 11:03:50 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+function setup(){
+    video=createCapture(VIDEO);
+    video.size(550,500);
+
+    canvas=createCanvas(550,550);
+    canvas.position(560,150);
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
+ }
+ 
+ function modelLoaded(){
+    console.log('PoseNet is initialized');
+ }
+ function gotPoses(results){
+    
+     if(results.length > 0){
+     console.log(results);
+
+    leftWristX = results[0].pose.leftWrist.x;
+    rightWristX = results[0].pose.rightWrist.x;
+    difference = floor(leftWristX - rightWristX);
+
+   }
+ }
+ 
+ function draw(){
+     background('#969A97');
+     textSize(difference);
+     fill('FFE787');
+     text('Peter',50,400);
+ }
